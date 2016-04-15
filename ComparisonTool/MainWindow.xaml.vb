a4e5f71cd44d6ec3863853db4bd0069f13172db9
George Hilliard <thirtythreeforty@gmail.com>
2016-04-15 13:39:19 -0500

    Get skeleton app working

--- ComparisonTool/MainWindow.xaml.vb
+++ ComparisonTool/MainWindow.xaml.vb
@@ -1,3 +1,25 @@
-﻿Class MainWindow
+﻿Imports Microsoft.Win32
+Imports Ookii.Dialogs.Wpf
 
+Class MainWindow
+    Private Sub keyTextBox_TextChanged(sender As Object, e As TextChangedEventArgs) _
+            Handles keyTextBox.TextChanged
+
+    End Sub
+
+    Private Sub chooseKeyButton_Click(sender As Object, e As RoutedEventArgs) _
+            Handles chooseKeyButton.Click
+        Dim dlg = New OpenFileDialog
+        dlg.Filter = "Word Documents (*.docx, *.doc)|*.docx;*.doc|All Files|*"
+        If dlg.ShowDialog Then
+            keyTextBox.Text = dlg.FileName
+        End If
+    End Sub
+
+    Private Sub chooseFolderButton_Click(sender As Object, e As RoutedEventArgs) Handles chooseFolderButton.Click
+        Dim dlg = New VistaFolderBrowserDialog
+        If dlg.ShowDialog Then
+            folderTextBox.Text = dlg.SelectedPath
+        End If
+    End Sub
 End Class
