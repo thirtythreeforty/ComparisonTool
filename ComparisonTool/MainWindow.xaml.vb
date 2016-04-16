a3aca89447e369631bc7418b6be9a2d29c4e333a
George Hilliard <thirtythreeforty@gmail.com>
2016-04-16 04:38:49 -0500

    Implement comparison for Word, Excel, and PowerPoint

--- ComparisonTool/MainWindow.xaml.vb
+++ ComparisonTool/MainWindow.xaml.vb
@@ -3,13 +3,17 @@ Imports Ookii.Dialogs.Wpf
 Imports System.IO
 
 Class MainWindow
-    Private worker = New WordWorker
-    Private currentFile As String
+    Private worker = New OfficeHandler
 
     Private Sub chooseKeyButton_Click(sender As Object, e As RoutedEventArgs) _
             Handles chooseKeyButton.Click
         Dim dlg = New OpenFileDialog
-        dlg.Filter = "Word Documents (*.docx, *.doc)|*.docx;*.doc|All Files|*"
+        dlg.Filter =
+            "All supported documents (*.docx, *.doc; *.xlsx, *.xls; *.pptx, *.ppt)|*.docx;*.doc;*.xlsx;*.xls;*.pptx;*.ppt|" +
+            "Word documents (*.docx, *.doc)|*.docx;*.doc|" +
+            "Excel documents (*.xlsx, *.xls)|*.xlsx;*.xls|" +
+            "PowerPoint documents (*.pptx, *.ppt)|*.pptx;*.ppt|" +
+            "All Files|*"
         If dlg.ShowDialog Then
             keyTextBox.Text = dlg.FileName
         End If
@@ -20,14 +24,6 @@ Class MainWindow
         Dim dlg = New VistaFolderBrowserDialog
         If dlg.ShowDialog Then
             folderTextBox.Text = dlg.SelectedPath
-        End If
-    End Sub
-
-    Private Sub compareFile(file As String)
-        Dim key = keyTextBox.Text
-        If My.Computer.FileSystem.FileExists(key) AndAlso
-           My.Computer.FileSystem.FileExists(file) Then
-            worker.compare(file)
         End If
     End Sub
 
@@ -43,50 +39,53 @@ Class MainWindow
 
     Private Async Sub filesComboBox_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) _
             Handles filesComboBox.SelectionChanged
-        updatePrevNext()
+        UpdatePrevNext()
 
         Dim sel = filesComboBox.SelectedItem
 
         If sel IsNot Nothing Then
-            comparingStatus()
+            ComparingStatus()
             ' Re-join the filename with the path name, since it was stripped
             ' to put it in the combobox
             Dim fullPath = Path.Combine(folderTextBox.Text, sel)
             Dim key = keyTextBox.Text
             Await Task.Run(Sub()
-                               worker.compare(key, fullPath)
+                               worker.Compare(key, fullPath)
                            End Sub)
-            updateStatus()
+            UpdateStatus()
         End If
     End Sub
 
     Private Sub folderTextBox_TextChanged(sender As Object, e As TextChangedEventArgs) _
             Handles folderTextBox.TextChanged
-        findWordFiles(folderTextBox.Text)
-        updateStatus()
+        FindSimilarFiles()
     End Sub
 
-    Private Sub findWordFiles(dir As String)
+    Private Sub FindSimilarFiles()
         filesComboBox.Items.Clear()
 
-        Try
-            Dim files = Directory.EnumerateFiles(folderTextBox.Text)
-            For Each file As String In files
-                Dim ext = Path.GetExtension(file)
-                Dim rootName = Path.GetFileName(file)
-                If ext = ".docx" Or ext = ".doc" Then
-                    filesComboBox.Items.Add(rootName)
-                End If
-            Next
-        Catch ex As Exception
-            ' Just let it pass, probably a bad directory name
-        End Try
+        If My.Computer.FileSystem.FileExists(keyTextBox.Text) Then
+            Try
+                Dim type = OfficeHandler.FindFileType(keyTextBox.Text)
+                Dim files = Directory.EnumerateFiles(folderTextBox.Text)
+                For Each file As String In files
+                    If OfficeHandler.FileTypeMatches(file, type) AndAlso
+                            file <> keyTextBox.Text Then
+                        Dim rootName = Path.GetFileName(file)
+                        filesComboBox.Items.Add(rootName)
+                    End If
+                Next
+            Catch ex As Exception
+                ' Just let it pass, probably a bad directory name
+            End Try
+        End If
 
         filesComboBox.SelectedIndex = -1
-        updatePrevNext()
+        UpdatePrevNext()
+        UpdateStatus()
     End Sub
 
-    Private Sub updatePrevNext()
+    Private Sub UpdatePrevNext()
         Dim idx = filesComboBox.SelectedIndex
         Dim keyPresent = My.Computer.FileSystem.FileExists(keyTextBox.Text)
 
@@ -94,33 +93,35 @@ Class MainWindow
         nextButton.IsEnabled = (idx < filesComboBox.Items.Count - 1 And keyPresent)
     End Sub
 
-    Private Sub updateStatus()
+    Private Sub UpdateStatus()
+        Dim numFound = filesComboBox.Items.Count
         If Not My.Computer.FileSystem.FileExists(keyTextBox.Text) Then
             statusTextBlock.Text = "Choose key"
         ElseIf Not My.Computer.FileSystem.DirectoryExists(folderTextBox.Text) Then
             statusTextBlock.Text = "Choose folder"
+        ElseIf numFound <= 0 Then
+            statusTextBlock.Text = "No matching files"
         Else
-            statusTextBlock.Text = "Ready"
+            statusTextBlock.Text = String.Format("Ready ({0} files)", numFound)
         End If
     End Sub
 
-    Private Sub comparingStatus()
+    Private Sub ComparingStatus()
         statusTextBlock.Text = "Comparing..."
     End Sub
 
     Private Sub keyTextBox_TextChanged(sender As Object, e As TextChangedEventArgs) _
             Handles keyTextBox.TextChanged
-        updateStatus()
-        updatePrevNext()
+        FindSimilarFiles()
     End Sub
 
     Private Sub aboutButton_Click(sender As Object, e As RoutedEventArgs) _
             Handles aboutButton.Click
         Dim dlg = New TaskDialog
-        dlg.MainInstruction = "Word Comparison Tool"
+        dlg.MainInstruction = "Office Comparison Tool"
         dlg.Content =
             "By George Hilliard (thirtythreeforty@gmail.com)" + Environment.NewLine +
-            "Last updated 15 April 2016" + Environment.NewLine +
+            "Last updated 16 April 2016" + Environment.NewLine +
             Environment.NewLine +
             "This program is free software: you can redistribute it and/or modify " +
             "it under the terms of the GNU General Public License as published by " +
---
+++ ComparisonTool/OfficeWorker.vb
@@ -0,0 +1,129 @@
+﻿Imports Microsoft.Office.Interop
+Imports System.Runtime.InteropServices
+Imports System.IO
+Imports System.Text.RegularExpressions
+
+Public Class OfficeHandler
+    Public Sub Compare(fileName As String, toCompare As String)
+        GetComparator(fileName)(fileName, toCompare)
+    End Sub
+
+    Public Enum FileType
+        Word
+        Excel
+        PowerPoint
+        Unknown
+    End Enum
+
+    Public Shared Function FileTypeMatches(file As String, type As FileType) As Boolean
+        Return FindFileType(file) = type AndAlso type <> FileType.Unknown
+    End Function
+
+    Public Shared Function FileTypeMatches(file1 As String, file2 As String) As Boolean
+        Dim file1type = FindFileType(file1)
+        Return file1type = FindFileType(file2) AndAlso file1type <> FileType.Unknown
+    End Function
+
+    Public Shared Function FindFileType(file As String) As FileType
+        If MatchesStrings(file, "doc", "docx") Then
+            Return FileType.Word
+        ElseIf MatchesStrings(file, "xls", "xlsx") Then
+            Return FileType.Excel
+        ElseIf MatchesStrings(file, "ppt", "pptx") Then
+            Return FileType.PowerPoint
+        Else
+            Return FileType.Unknown
+        End If
+    End Function
+
+    Private Shared Function MatchesStrings(file As String, s1 As String, s2 As String) As Boolean
+        Dim ext = Path.GetExtension(file)
+        Return ext.ToLower().EndsWith(s1) OrElse ext.EndsWith(s2)
+    End Function
+
+    Private Delegate Sub Comparator(base As String, fork As String)
+
+    Private Function GetComparator(file As String) As Comparator
+        If FileTypeMatches(file, FileType.Word) Then
+            Return AddressOf WordCompare
+        ElseIf FileTypeMatches(file, FileType.Excel) Then
+            Return AddressOf ExcelCompare
+        ElseIf FileTypeMatches(file, FileType.PowerPoint) Then
+            Return AddressOf PowerPointCompare
+        Else
+            Return Sub(x As String, y As String)
+                   End Sub
+        End If
+    End Function
+
+    Private Sub WordCompare(base As String, fork As String)
+        Dim app As Word.Application
+        Try
+            app = Marshal.GetActiveObject("Word.Application")
+        Catch ex As Exception
+            app = New Word.Application
+        End Try
+        Dim doc = app.Documents.Open(base)
+        If doc IsNot Nothing Then
+            doc.Compare(fork)
+            doc.Close()
+            app.Visible = True
+        End If
+    End Sub
+
+    Private excelCompareBinary = New Lazy(Of String)(
+        Function() As String
+            Dim officeLocation = Path.Combine(
+                My.Computer.FileSystem.SpecialDirectories.ProgramFiles,
+                "Microsoft Office"
+            )
+            Dim foundCompares =
+                My.Computer.FileSystem.GetFiles(officeLocation,
+                                                FileIO.SearchOption.SearchAllSubDirectories,
+                                                "SPREADSHEETCOMPARE.EXE")
+            If foundCompares.Count = 0 Then
+                Return Nothing
+            End If
+            Return foundCompares.ElementAt(0)
+        End Function)
+    Private Sub ExcelCompare(base As String, fork As String)
+        ' Excel does not expose its Inquire add-in that provides spreadsheet
+        ' diff via an API.  We must call it manually as a standalone program. See
+        ' http://stackoverflow.com/questions/13702663 for details.
+        Dim compareBinary = excelCompareBinary.Value
+        If compareBinary Is Nothing Then
+            MessageBox.Show("Inquire Add-In for Excel seems to be missing! " +
+                            "This is only shipped with Professional Plus versions of Office :(")
+            Return
+        End If
+
+        ' So this is new levels of horrifying.  The arguments are not passed via the command
+        ' line -- that would be too straightforward.  No, they are instead written to a file
+        ' which is then passed to the comparison program, which deletes the (hopefully
+        ' temporary) argument file.  The comparison program crashes if anything is out of place.
+        Dim tempFileLocation = Path.Combine(
+            My.Computer.FileSystem.SpecialDirectories.Temp,
+            "ComparisonToolExcel" + Rnd().ToString() + ".txt"
+        )
+        Dim compareList = base + Environment.NewLine + fork
+        My.Computer.FileSystem.WriteAllText(tempFileLocation, compareList, False)
+
+        Dim proc = Process.Start(compareBinary, EscapePathForShell(tempFileLocation))
+    End Sub
+
+    Private Sub PowerPointCompare(base As String, fork As String)
+        Dim app As PowerPoint.Application
+        Try
+            app = Marshal.GetActiveObject("PowerPoint.Application")
+        Catch ex As Exception
+            app = New PowerPoint.Application
+        End Try
+        app.Visible = True
+        Dim doc = app.Presentations.Open(base)
+        doc.Merge(fork)
+    End Sub
+
+    Private Shared Function EscapePathForShell(s As String) As String
+        Return """" + Regex.Replace(s, "(\\+)$", "$1$1") + """"
+    End Function
+End Class
